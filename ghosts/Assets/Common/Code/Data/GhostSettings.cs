4d0c90d4e22dbe7d6576ffea96a7f13da07e9a07
Yehor Kostenko <yehor.kostenko.wow@gmail.com>
2022-10-27 17:28:14 +0300

    GhostData added to config limit of the ghosts on the screen

---
+++ ghosts/Assets/Common/Code/Data/GhostSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Common.Code.Data
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "GhostSettings")]
+    public class GhostSettings: ScriptableObject
+    {
+        public int GhostCount;
+    }
+}
